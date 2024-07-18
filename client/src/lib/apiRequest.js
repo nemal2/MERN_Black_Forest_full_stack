29adb35669b2654557f86db148775b1a46bec436
Nemal Perera <122453475+nemal2@users.noreply.github.com>
2024-07-19 02:22:29 +0530

    baseurl edit

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL:"http://localhost:8800/api",
+    baseURL:"https://blackapi.vercel.app",
+    //http://localhost:8800/api
     withCredentials : true,
 })
 

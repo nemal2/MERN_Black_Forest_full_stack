7a284ba895566efe4182c3013e4993d05d75d8e5
Nemal Perera <122453475+nemal2@users.noreply.github.com>
2024-07-20 01:31:13 +0530

    socket deploy

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    setSocket(io("https://mern-black-forest-socket.onrender.com"));
   }, []);
 
   useEffect(() => {

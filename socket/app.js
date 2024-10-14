9c79f80dd1d5bb4fe0e80c4d965b2134e6905e5d
Nemal Perera <122453475+nemal2@users.noreply.github.com>
2024-10-14 18:18:53 +0530

    Update app.js

--- socket/app.js
+++ socket/app.js
@@ -1,15 +1,10 @@
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import express from 'express';
+import { Server, Socket } from "socket.io";
 
-const app = express();
-const httpServer = createServer(app);
-
-const io = new Server(httpServer, {
+const io = new Server({
   cors: {
     origin: [
       //"http://localhost:5173", // Development origin
-      "https://black-forest-agent-service-cli.vercel.app" // Production origin
+      "https://black-forest.vercel.app" // Production origin
     ],
   },
 });
@@ -25,32 +20,25 @@ const addUser = (userId, socketId) => {
 
 const removeUser = (socketId) => {
   onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
-};
+}
 
 const getUser = (userId) => {
   return onlineUser.find((user) => user.userId === userId);
-};
+}
 
 io.on("connection", (socket) => {
   socket.on("test", (userId) => {
     addUser(userId, socket.id);
   });
 
-  socket.on("sendMessage", ({ receiverId, data }) => {
+  socket.on("sendMessage", ({receiverId, data})=>{
     console.log(receiverId);
-    const receiver = getUser(receiverId);
-    if (receiver) {
-      io.to(receiver.socketId).emit("getMessage", data);
-    }
-  });
+    io.to(receiver.socketId).emit("getMessage", data);
+  })
 
-  socket.on("disconnect", () => {
-    removeUser(socket.id);
-  });
+  socket.on("disconnect", ()=> {
+    removeUser(socket.id)
+  })
 });
 
-const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+io.listen("4000");
